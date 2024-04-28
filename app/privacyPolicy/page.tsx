3530f5c27b65051499ca1e68d55a5e9c64858b0b
Saikou Sow <sow.coding@gmail.com>
2024-04-28 22:42:14 +0200

    privacy and terms

--- app/privacyPolicy/page.tsx
+++ app/privacyPolicy/page.tsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React from 'react';
 
-function PrivacyPolicy() {
+export default function PrivacyPolicyPage() {
   return (
-    <>
-        
-    </>
-  )
-}
+    <div className='w-full flex flex-col items-center'>
+    <main className='w-4/5 flex flex-col items-center'>
+      <h1 className='text-center font-bold text-2xl mt-4'>Privacy policy - getleads.dev</h1>
+      <div className="my-4">
+      <h2 className="text-center text-xl">Privacy Policy</h2>
+      <p className="text-center">
+        At getleads.dev, we are committed to protecting the privacy and security of our {`users'`} information. This policy outlines our practices regarding the collection, use, and disclosure of information we receive when you use our services.
+      </p>
 
-export default PrivacyPolicy
+      <section className="my-4">
+        <h2 className="text-center text-xl">Information Collection</h2>
+        <p className="text-center">
+          getleads.dev collects information solely related to the functionalities of our service. This includes search filters used by users and the results of such searches. We collect this information to enhance our services and to provide you with tailored functionalities.
+        </p>
+      </section>
+
+      <section className="my-4">
+        <h2 className="text-center text-xl">Use of Information</h2>
+        <p className="text-center">
+          The information collected by getleads.dev is used to:
+        </p>
+        <ul className="text-center">
+          <li>Improve and customize our services and functionalities;</li>
+          <li>Understand and analyze the usage trends and preferences of our users;</li>
+          <li>Develop new products, services, features, and functionalities.</li>
+        </ul>
+      </section>
+
+      <section className="my-4">
+        <h2 className="text-center text-xl">Information Sharing and Disclosure</h2>
+        <p className="text-center">
+          getleads.dev does not sell, trade, or rent your personal information to others. We do not share your information with third parties except as described in this Privacy Policy. The information collected is for internal use only to improve the services provided by getleads.dev.
+        </p>
+      </section>
+
+      <section className="my-4">
+        <h2 className="text-center text-xl">Legal Compliance</h2>
+        <p className="text-center">
+          getleads.dev may disclose your information if required to do so by law or in the good faith belief that such action is necessary to:
+        </p>
+        <ul className="text-center">
+          <li>Comply with a legal obligation;</li>
+          <li>Protect and defend the rights or property of getleads.dev;</li>
+          <li>Prevent or investigate possible wrongdoing in connection with the service;</li>
+          <li>Protect the personal safety of users of the service or the public;</li>
+          <li>Protect against legal liability.</li>
+        </ul>
+        <p className="text-center">
+          Such disclosures may be made pursuant to judicial or other government subpoenas, warrants, or orders, or in accordance with applicable law.
+        </p>
+      </section>
+
+      <section className="my-4">
+        <h2 className="text-center text-xl">Security</h2>
+        <p className="text-center">
+          We take reasonable measures to protect the information we collect from or about you from unauthorized access, misuse, or disclosure. Please be aware, however, that no method of transmitting information over the Internet or storing information is completely secure.
+        </p>
+      </section>
+
+      <section className="my-4">
+        <h2 className="text-center text-xl">Changes to This Privacy Policy</h2>
+        <p className="text-center">
+          We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page. You are advised to review this Privacy Policy periodically for any changes.
+        </p>
+      </section>
+
+      <section className="my-4">
+        <h2 className="text-center text-xl">Contact Us</h2>
+        <p className="text-center">
+          If you have any questions about this Privacy Policy, please contact us at support@swos.be.
+        </p>
+      </section>
+    </div>
+    </main>
+    </div>
+  );
+}
--- app/termsAndConditions/page.tsx
+++ app/termsAndConditions/page.tsx
@@ -1,9 +1,104 @@
-import React from 'react'
-
-function TermsAndConditions() {
+export default function Terms() {
   return (
-    <div>TermsAndConditions</div>
-  )
-}
+    <div className='flex flex-col items-center w-full'>
+      <main className="flex flex-col items-center w-4/5">
+        <h1 className='text-center font-bold text-2xl'>Terms of Use - getleads.dev</h1>
+        <section className="my-4">
+          <h2 className="text-center text-xl">Introduction</h2>
+          <p className="text-center">
+            Welcome to getleads.dev. By using our platform, you gain access to a prospecting tool dedicated to developers and digital marketing professionals. These Terms of Use govern your access to and use of all services and products offered by GetLeads.dev.
+          </p>
+        </section>
 
-export default TermsAndConditions
+        <section className="my-4">
+          <h2 className="text-center text-xl">Acceptance of Terms</h2>
+          <p className="text-center">
+            By using our site, you confirm that you have read, understood, and agreed to be bound by these Terms of Use. If you do not accept these terms, you must not access or use GetLeads.dev.
+          </p>
+        </section>
+
+        <section className="my-4">
+          <h2 className="text-center text-xl">Use of Services</h2>
+          <p className="text-center">
+            GetLeads.dev uses third-party APIs such as Apollo.io, Wappalyzer, and Hunter.io to enhance the data offered. Your use of our platform implies your consent for us to interact with these services.
+          </p>
+        </section>
+
+        <section className="my-4">
+          <h2 className="text-center">Age Restrictions</h2>
+          <p className="text-center">
+            The services of GetLeads.dev are not intended for individuals under the age of 18. If you are under 18, you are not permitted to use GetLeads.dev.
+          </p>
+        </section>
+
+        <section className="my-4">
+          <h2 className="text-center text-xl">Changes to Terms</h2>
+          <p className="text-center">
+            GetLeads.dev reserves the right to modify these terms at any time. We will notify you of any significant changes by notification on our site or by email. Your continued use of the site after these changes constitutes your acceptance of the new terms.
+          </p>
+        </section>
+
+        <section className="my-4">
+          <h2 className="text-center text-xl">Refunds</h2>
+          <p className="text-center">
+          If the product does not meet your expectations, it is possible to request a refund by contacting the support email address (
+          support@swos.be and by stipulating in the subject of the email: {`"REFUND"`}) in 7 days maximum by proving extensive use of getleads.dev as part of your prospecting which led to nothing, Proofs may be requested and an internal audit will obviously be carried out to verify your use of getleads.dev. If it turns out that your case is real, a full refund is then possible.
+          </p>
+        </section>
+
+        <section className="my-4">
+          <h2 className="text-center text-xl">Intellectual Property</h2>
+          <p className="text-center">
+            All content displayed on GetLeads.dev, including texts, graphics, logos, images, data compilations, and software, is the property of GetLeads.dev or its content suppliers and is protected by international copyright laws.
+          </p>
+        </section>
+
+        <section className="my-4">
+          <h2 className="text-center text-xl">Limitation of Liability</h2>
+          <p className="text-center">
+            GetLeads.dev does not guarantee the accuracy, relevance, or completeness of the data provided and will not be responsible for errors, omissions, or other inaccuracies in the information provided.
+          </p>
+        </section>
+
+        <section className="my-4">
+          <h2 className="text-center text-xl">Prohibited Uses</h2>
+          <p className="text-center">
+            As a user of getleads.dev, you agree not to use the platform for any purposes that are unlawful or prohibited by these Terms. This includes, but is not limited to, the following activities:
+          </p>
+          <ul className="text-center">
+            <li className="text-center">Engaging in any form of spamming or unsolicited communication.</li>
+            <li className="text-center">Using getleads.dev to conduct or promote any illegal activities.</li>
+            <li className="text-center">Using the platform to create, host, or transmit content that is malicious, abusive, harassing, tortious, defamatory, vulgar, obscene, invasive of {`another's`} privacy, hateful, or racially or ethnically objectionable.</li>
+            <li className="text-center">Attempting to interfere with, compromise the system integrity or security, or decipher any transmissions to or from the servers running the platform.</li>
+            <li className="text-center">Uploading invalid data, viruses, worms, or other software agents through the platform.</li>
+            <li className="text-center">Collecting or harvesting any personally identifiable information from the platform, including account names, without proper consent.</li>
+            <li className="text-center">Impersonating another person or otherwise misrepresenting your affiliation with a person or entity, conducting fraud, hiding or attempting to hide your identity.</li>
+            <li className="text-center">Interfering with the proper working of the platform.</li>
+          </ul>
+          <p className="text-center">
+            Any use of getleads.dev contrary to our mission of providing lead generation and prospecting tools, including for competing purposes, will be considered a breach of these Terms and may result in the immediate termination of your account without notice.
+          </p>
+      </section>
+
+      <section className="my-4">
+        <h2 className="text-center text-xl">Limitation of Liability</h2>
+        <p className="text-center">
+          getleads.dev, its owners, employees, and affiliates will not be liable for any indirect, incidental, special, consequential or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from:
+        </p>
+        <ul className="text-center">
+          <li className="text-center">Your access to or use of or inability to access or use the services;</li>
+          <li className="text-center">Any conduct or content of any third party on the services;</li>
+          <li className="text-center">Any content obtained from the services;</li>
+          <li className="text-center">Unauthorized access, use or alteration of your transmissions or content, whether based on warranty, contract, tort (including negligence) or any other legal theory, whether or not we have been informed of the possibility of such damage, and even if a remedy set forth herein is found to have failed of its essential purpose.</li>
+        </ul>
+        <p className="text-center">
+          You acknowledge that getleads.dev employs various measures to ensure the security of its services, but recognize that no method of electronic storage and transmission over the Internet is completely secure. Therefore, while getleads.dev strives to use commercially acceptable means to protect your information, it cannot guarantee its absolute security and assumes no liability for any disclosure of data due to errors in transmission, unauthorized third-party access, or other acts beyond its control.
+        </p>
+        <p className="text-center">
+          You agree that the use of getleads.dev is at your own risk and that you are solely responsible for any damages to your computer system or loss of data that results from any material and/or data downloaded from or otherwise provided through getleads.dev.
+        </p>
+      </section>
+      </main>
+    </div>
+  );
+}
